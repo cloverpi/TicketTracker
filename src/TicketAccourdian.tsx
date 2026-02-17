e8d4532d9d385d81183f63b010b69d261af03448
Chris Humber <christopher.humber@gmail.com>
2026-02-17 16:57:50 -0600

    Accordian text length test

--- src/TicketAccourdian.tsx
+++ src/TicketAccourdian.tsx
@@ -78,12 +78,7 @@ function TicketAccourdian() {
             data-bs-parent="#accordionFlushExample"
           >
             <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the third item’s accordion body. Nothing more exciting happening
-              here in terms of content, but just filling up the space to make it
-              look, at least at first glance, a bit more representative of how
-              this would look in a real-world application.
+              Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application of how this would look in a real-world application.of how this would look in a real-world application.of how this would look in a real-world.
             </div>
           </div>
         </div>
